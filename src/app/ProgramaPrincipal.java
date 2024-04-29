0975aad981820d1121487b1f49d936005faae5d3
Genilson <g.oneg20@gmail.com>
2024-04-29 19:46:05 -0300

    Classe Pessoa e falha em teste com classe em outro pacote

---
+++ src/app/ProgramaPrincipal.java
@@ -0,0 +1,8 @@
+package app;
+
+public class ProgramaPrincipal {
+    public static void main(String[] args) {
+        Pessoa fulano = new Pessoa();
+//        System.out.println(fulano.nome);
+    }
+}
