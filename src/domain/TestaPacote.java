36a286cb0e4be28c17bdfb0085787dcdead3a973
Genilson <g.oneg20@gmail.com>
2024-04-29 19:37:06 -0300

    Classe Pessoa e respectivo teste dentro do mesmo pacote

---
+++ src/domain/TestaPacote.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class TestaPacote {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+        p.nome ="ze da silva";
+        System.out.println(p.nome);
+    }
+}
